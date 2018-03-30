8eb67a158414b81d6f7c54e5bdcb3b429ccd7e6e
Daniel Sakuma <dsakuma@gmail.com>
2018-03-30 19:05:17 -0300

    Add mutex lock

--- mini-ep2/mini-ep2.c
+++ mini-ep2/mini-ep2.c
@@ -3,9 +3,10 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-
+#define MAX_JUMPS 1000
 int counter = 0;
 char **arrayStringLake;
+pthread_mutex_t lock;
 
 
 void initializeStringLake(int numFrogs, int lakeSize){
@@ -26,6 +27,54 @@ void seedRand() {
   srand(seed);
 }
 
+void printLakeState(lakeSize){
+  for(int i = 0; i < lakeSize; i++)
+  {
+     printf("%s\n", arrayStringLake[i]);
+  }
+}
+
+void tryJump(long threadid){
+  printf("O sapo %d tentando pular, no counter %d!\n", threadid, counter);
+}
+
+void *frog(void *threadid){
+   long tid = threadid;
+
+   while(counter < MAX_JUMPS){
+     pthread_mutex_lock(&lock);
+     usleep(rand()%100);
+     if(counter < MAX_JUMPS){
+       tryJump(threadid);
+       counter++;
+     };
+     pthread_mutex_unlock(&lock);
+   }
+   pthread_exit(NULL);
+}
+
+void initializeThreads(numFrogs, lakeSize){
+  if (pthread_mutex_init(&lock, NULL) != 0)
+  {
+      printf("\n mutex init failed\n");
+      return;
+  }
+
+  pthread_t threads[lakeSize];
+  int error_code;
+  for(long t = 0;t < lakeSize; t++){
+      if(t != numFrogs+1){
+        printf("initializing thread %d\n", t);
+        error_code = pthread_create(&threads[t], NULL, frog, (void *) t);
+        if (error_code){
+            printf("ERROR; return code from pthread_create() is %d\n", error_code);
+            exit(-1);
+        };
+      }
+  };
+
+  pthread_mutex_destroy(&lock);
+}
 
 int main(int argc, char *argv[]){
   printf("=====Initializing=====\n");
@@ -35,12 +84,12 @@ int main(int argc, char *argv[]){
   int lakeSize = (numFrogs * 2) + 1;
 
   initializeStringLake(numFrogs, lakeSize);
-  for(int i = 0; i < lakeSize; i++)
-  {
-     printf("%s\n", arrayStringLake[i]);
-  }
+  // printLakeState(lakeSize);
+  initializeThreads(numFrogs, lakeSize);
 
-  strcpy(arrayStringLake[0], "M-1");
+
+
+  pthread_exit(NULL);
   printf("======Finished======\n");
 };
 
