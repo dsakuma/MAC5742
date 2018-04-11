8e9e9d6fee0daca340c962bbe4007a099a80985a
Daniel Sakuma <dsakuma@gmail.com>
2018-04-11 13:57:20 -0300

    [WIP] Generate histogram

--- mini-ep3/call_contention.sh
+++ mini-ep3/call_contention.sh
@@ -1,31 +1,83 @@
 #!/bin/bash
 
-echo "CALL CONTENTION"
-echo "---------------"
+call_contention()
+{
+  echo ""
+  echo "CALL CONTENTION"
+  echo "---------------"
+  # Call contention with different numbers of Ts and Ns
+  T=( 1700 1800 1900 2000 )
+  N=( 10 )
 
-T=( 2 20 200 2000 )
-N=( 10 1000 100000 10000000 )
-
-for t in "${T[@]}"
-do
-  for n in "${N[@]}"
+  for t in "${T[@]}"
   do
-     printf "\nRunning for T=$t and N=$n\n"
-     output=$(./contention.sh $n $t)
+    for n in "${N[@]}"
+    do
+       printf "\nRunning for T=$t and N=$n\n"
+       output=$(./contention.sh $n $t)
 
-     read N_IFS <<< $(echo "$output" | awk '/Number of ifs:[[:space:]]/ { print $4 }')
-     read TIME_IFS <<< $(echo "$output" | awk '/[[:space:]]Average of[[:space:]]/ { print $5 }')
+       read N_IFS <<< $(echo "$output" | awk '/Number of ifs:[[:space:]]/ { print $4 }')
+       read TIME_IFS <<< $(echo "$output" | awk '/[[:space:]]Average of[[:space:]]/ { print $5 }')
 
-     # echo "${N_IFS}"
-     echo "${TIME_IFS}"
+       # echo "${N_IFS}"
+       echo "${TIME_IFS}"
+    done
   done
-done
+}
 
-# declare -a arr=(1 2 3)
+index_of_best()
+{
+  echo 0
+}
 
-# arr=$(echo $N_IFS | tr " " "\n")
-#
-# for i in $arr
-# do
-#   printf "Running for i=$i\n"
-# done
+index_of_worst()
+{
+  echo 9
+}
+
+print_histogram_data()
+{
+  echo ""
+  echo "BEST HISTOGRAM DATA"
+  echo "-------------------"
+  for i in `seq 0 9`
+  do
+    printf "$i - ${BESTHIST[i]}\n"
+  done
+
+  echo ""
+  echo "BEST HISTOGRAM DATA"
+  echo "-------------------"
+  for i in `seq 0 9`
+  do
+    printf "$i - ${WORSTHIST[i]}\n"
+  done
+}
+
+generate_histogram()
+{
+  echo ""
+  echo "GENERATE HISTOGRAM DATA"
+  echo "-----------------------"
+  # Collect data to generate a histogram for a specific number of T and N
+  BESTHIST=( 0 0 0 0 0 0 0 0 0 0 )
+  WORSTHIST=( 0 0 0 0 0 0 0 0 0 0 )
+  T=2
+  N=10
+  counter=0
+  N_ROUNDS=2
+  for i in `seq 1 $N_ROUNDS`
+  do
+      printf "Running for T=$T and N=$N. Round: $i\n"
+      output=$(./contention.sh $N $T)
+      read TIME_IFS <<< $(echo "$output" | awk '/[[:space:]]Average of[[:space:]]/ { print $5 }')
+      idx_best=$(index_of_best)
+      idx_worst=$(index_of_worst)
+      BESTHIST[$idx_best]=$((${BESTHIST[$idx_best]}+1))
+      WORSTHIST[$idx_worst]=$((${WORSTHIST[$idx_worst]}+1))
+  done
+}
+
+# call_contention
+generate_histogram
+print_histogram_data
