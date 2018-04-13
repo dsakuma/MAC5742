7d8c672b1c87864a06ad49e0789abfb57db26630
Daniel Sakuma <dsakuma@gmail.com>
2018-04-12 22:12:11 -0300

    Add n rounds as a parameter

--- mini-ep3/call_contention.sh
+++ mini-ep3/call_contention.sh
@@ -2,9 +2,9 @@
 
 index_of_best()
 {
+  # Logic to select the index with the best time
   current_best=0
   for i in "${!TIME_IFS[@]}"; do
-    # printf "%s\t%s\n" "$i" "${TIME_IFS[$i]}"
     if [ $(echo "${TIME_IFS[$i]} < ${TIME_IFS[$current_best]}" | bc) -eq 1 ]; then
       current_best=$i
     fi
@@ -14,9 +14,9 @@ index_of_best()
 
 index_of_worst()
 {
+  # Logic to select the index with the worst time
   current_worst=0
   for i in "${!TIME_IFS[@]}"; do
-    # printf "%s\t%s\n" "$i" "${TIME_IFS[$i]}"
     if [ $(echo "${TIME_IFS[$i]} > ${TIME_IFS[$current_worst]}" | bc) -eq 1 ]; then
       current_worst=$i
     fi
@@ -26,6 +26,7 @@ index_of_worst()
 
 print_time_ifs()
 {
+  # Print the array with if times
   echo ""
   echo "PRINT TIME IFS"
   echo "--------------"
@@ -37,6 +38,7 @@ print_time_ifs()
 
 print_histogram_data()
 {
+  # Print the histogram data
   echo ""
   echo "BEST HISTOGRAM DATA"
   echo "-------------------"
@@ -56,16 +58,16 @@ print_histogram_data()
 
 generate_histogram()
 {
+  # Collect data to generate a histogram for a specific number of T and N
   echo ""
   echo "GENERATE HISTOGRAM DATA"
   echo "-----------------------"
-  # Collect data to generate a histogram for a specific number of T and N
   BESTHIST=( 0 0 0 0 0 0 0 0 0 0 )
   WORSTHIST=( 0 0 0 0 0 0 0 0 0 0 )
   N=$1
   T=$2
+  N_ROUNDS=$3
   counter=0
-  N_ROUNDS=1000
   for i in `seq 1 $N_ROUNDS`
   do
       printf "Running for T=$T and N=$N. Round: $i\n"
@@ -80,11 +82,14 @@ generate_histogram()
   done
 }
 
-generate_histogram 10000 250
+generate_histogram 10000 25 5
 print_histogram_data
 
-generate_histogram 10000 500
-print_histogram_data
-
-generate_histogram 10000 1000
-print_histogram_data
+# generate_histogram 10000 250 1000
+# print_histogram_data
+#
+# generate_histogram 10000 500 1000
+# print_histogram_data
+#
+# generate_histogram 10000 1000 1000
+# print_histogram_data
