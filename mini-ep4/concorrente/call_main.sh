d2ca3f86524ef828ebab837e607ec9b9632c172f
Daniel Sakuma <dsakuma@gmail.com>
2018-05-04 09:11:55 -0300

    Add report and improve output

--- mini-ep4/concorrente/call_main.sh
+++ mini-ep4/concorrente/call_main.sh
@@ -8,9 +8,6 @@ parse_test_output()
   #echo "$test_output"
 }
 
-#count_bottom_outlier()
-#count_top_outlier()
-
 store_results()
 {
   algorithm=$(echo "$test_output" | awk '/Algorithm:[[:space:]]/ { print $2 }')
@@ -30,62 +27,77 @@ store_results()
   fi
 }
 
-print_statistics()
+generate_statistics()
 {
-  echo ""
-  echo "PRINT STATISTICS"
-  echo "================"
-
+  echo "Generating statistics..."
   number_of_tests_per_algorith=$((${number_of_tests} / 2))
 
   mean_bakery_sum_elapsed_time=$((${bakery_sum_elapsed_time} / ${number_of_tests_per_algorith}))
   mean_bakery_sum_std_deviation_access=$(echo "scale=2; ${bakery_sum_std_deviation_access} / ${number_of_tests_per_algorith}" | bc)
   mean_gate_sum_elapsed_time=$((${gate_sum_elapsed_time} / ${number_of_tests_per_algorith}))
   mean_gate_sum_std_deviation_access=$(echo "scale=2; ${gate_sum_std_deviation_access} / ${number_of_tests_per_algorith}" | bc)
+}
 
+print_statistics()
+{
+  echo "\nResults:"
   echo "Bakery - Mean Elapsed Time: $mean_bakery_sum_elapsed_time"
   echo "Bakery - Mean Standard Deviation Access: $mean_bakery_sum_std_deviation_access"
   echo "Gate - Mean Elapsed Time: $mean_gate_sum_elapsed_time"
   echo "Gate - Mean Standard Deviation Access: $mean_gate_sum_std_deviation_access"
-
 }
 
 compile_project()
 {
   # Ensure the project is compiled
   echo ""
-  echo "COMPILE PROJECT"
-  echo "==============="
+  echo "Compiling project..."
   make
 }
 
-generate_statistics()
+run_main()
 {
-  # Generate statistics for a specific number of threads and time consuming cpu
-  echo ""
-  echo "GENERATE STATISTICS"
-  echo "==================="
+  echo "Running main.c..."
+  output=$(./main ${NUM_THREADS} ${TOTAL_TIME})
+}
+
+run_program()
+{
+  # Run program for a specific number of threads and time consuming cpu
+  echo "---------------------"
+  echo "RUN PROGRAM (n_thread=$1, total_time=$2)"
   NUM_THREADS=$1
   TOTAL_TIME=$2
   bakery_sum_elapsed_time=0
   bakery_sum_std_deviation_access=0
   gate_sum_elapsed_time=0
   gate_sum_std_deviation_access=0
-  output=$(./main ${NUM_THREADS} ${TOTAL_TIME})
+  run_main
   number_of_lines=$(echo "$output" | wc -l)
   number_of_tests=$((${number_of_lines} / $OUTPUT_LINES_PER_TEST))
+  echo "Parsing main.c output..."
   for test_number in `seq 1 $number_of_tests`
   do
-    echo "Processing test ${test_number}"
     parse_test_output
     store_results
   done
+  generate_statistics
   print_statistics
+  echo "---------------------"
 }
+
+#count top outlier
+#ar=(10 30 44 44 69 12 11)
+#IFS=$'\n'
+#echo "${ar[*]}" | sort -nr | head -n1
+
 
 compile_project
 
+run_program 10 300000
 
-#generate_statistics 10 300000
+run_program 10 3000000
 
-generate_statistics 10 3000000
+run_program 5 300000
+
+run_program 5 3000000
