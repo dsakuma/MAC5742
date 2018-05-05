3a953865383757c87ac1a69b8a77cae241cfdac3
Daniel Sakuma <dsakuma@gmail.com>
2018-05-04 22:51:33 -0300

    Count outliers

--- mini-ep4/concorrente/call_main.sh
+++ mini-ep4/concorrente/call_main.sh
@@ -22,6 +22,50 @@ store_results()
   fi
 }
 
+count_outliers()
+{
+  parsed_access=$(echo "$test_output" | awk '/^[0-9]/ {print substr($0, 1, length($0)-2)}')
+  IFS=', ' read -a access_count <<< $parsed_access
+  max=${access_count[0]}
+  min=${access_count[0]}
+  for i in "${access_count[@]}"
+  do
+      # Update max if applicable
+      if [[ "$i" -gt "$max" ]]; then
+          max="$i"
+      fi
+
+      # Update min if applicable
+      if [[ "$i" -lt "$min" ]]; then
+          min="$i"
+      fi
+  done
+  two_std_deviation=$(echo "${std_deviation_access} * 3" | bc)
+  top_limit=$(echo "${avg_access} + ${two_std_deviation}" | bc)
+  bottom_limit=$(echo "${avg_access} - ${two_std_deviation}" | bc)
+
+  if [ "$algorithm" = "bakery," ]; then
+    if [ $(echo "${max} > ${top_limit}" | bc) -eq 1 ]; then
+      #echo $max
+      #echo $top_limit
+      bakery_top_outlier_counter=$((${bakery_top_outlier_counter} + 1))
+    fi
+    if [ $(echo "${min} < ${bottom_limit}" | bc) -eq 1 ]; then
+      bakery_bottom_outlier_counter=$((${bakery_bottom_outlier_counter} + 1))
+    fi
+  else
+    if [ $(echo "${max} > ${top_limit}" | bc) -eq 1 ]; then
+      gate_top_outlier_counter=$((${gate_top_outlier_counter} + 1))
+    fi
+    if [ $(echo "${min} < ${bottom_limit}" | bc) -eq 1 ]; then
+      gate_bottom_outlier_counter=$((${gate_bottom_outlier_counter} + 1))
+    fi
+  fi
+
+  #echo "Max is: $max"
+  #echo "Min is: $min"
+}
+
 generate_statistics()
 {
   echo "Generating statistics..."
@@ -40,6 +84,12 @@ print_statistics()
   echo "Bakery - Mean Standard Deviation Access: $mean_bakery_sum_std_deviation_access"
   echo "Gate - Mean Elapsed Time: $mean_gate_sum_elapsed_time"
   echo "Gate - Mean Standard Deviation Access: $mean_gate_sum_std_deviation_access"
+
+  echo "Bakery - Top Outlier Count: $bakery_top_outlier_counter"
+  echo "Bakery - Bottom Outlier Count: $bakery_bottom_outlier_counter"
+
+  echo "Gate - Top Outlier Count: $gate_top_outlier_counter"
+  echo "Gate - Bottom Outlier Count: $gate_bottom_outlier_counter"
 }
 
 compile_project()
@@ -67,6 +117,10 @@ run_program()
   bakery_sum_std_deviation_access=0
   gate_sum_elapsed_time=0
   gate_sum_std_deviation_access=0
+  bakery_top_outlier_counter=0
+  bakery_bottom_outlier_counter=0
+  gate_top_outlier_counter=0
+  gate_bottom_outlier_counter=0
   run_main
   number_of_lines=$(echo "$output" | wc -l)
   number_of_tests=$((${number_of_lines} / $OUTPUT_LINES_PER_TEST))
@@ -75,17 +129,12 @@ run_program()
   do
     parse_test_output
     store_results
+    count_outliers
   done
   generate_statistics
   print_statistics
   echo "---------------------"
 }
-
-#count top outlier
-#ar=(10 30 44 44 69 12 11)
-#IFS=$'\n'
-#echo "${ar[*]}" | sort -nr | head -n1
-
 
 compile_project
 
