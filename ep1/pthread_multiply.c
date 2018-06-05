760f50acb1b964f864414c0aee90b942c08bc389
Daniel Sakuma <dsakuma@gmail.com>
2018-06-05 17:30:10 -0300

    Multiply quadratic matrices using pthread

--- ep1/pthread_multiply.c
+++ ep1/pthread_multiply.c
@@ -5,20 +5,47 @@
 #include "sequential_multiply.h"
 #include <pthread.h>
 
+int num_threads = 1;
+
 struct thread_data {
  int thread_id;
  double **matrix_a;
+ double **matrix_b;
+ double **matrix_c;
 };
-
 
 void * worker( void *threadarg )
 {
   struct thread_data *my_data;
   my_data = (struct thread_data *) threadarg;
-  int taskid = my_data->thread_id;
+  int tid = my_data->thread_id;
   double **matrix_a = my_data->matrix_a;
-  printf("taskid->%d\n", taskid);
+  double **matrix_b = my_data->matrix_b;
+  double **matrix_c = my_data->matrix_c;
 
+  printf("tid->%d\n", tid);
+
+  int i, j, k, portion_size, row_start, row_end;
+  double sum;
+  int size = 2;
+
+  portion_size = size / num_threads;
+  row_start = tid * portion_size;
+  row_end = (tid+1) * portion_size;
+
+  // printf("portion_size->%d\n", portion_size);
+
+  for (i = row_start; i < row_end; ++i) { // hold row index of 'matrix1'
+  for (j = 0; j < size; ++j) { // hold column index of 'matrix2'
+    sum = 0; // hold value of a cell
+    /* one pass to sum the multiplications of corresponding cells
+ in the row vector and column vector. */
+    for (k = 0; k < size; ++k) {
+        sum += matrix_a[ i ][ k ] * matrix_b[ k ][ j ];
+    }
+    matrix_c[ i ][ j ] = sum;
+  }
+}
 
 
   return NULL;
@@ -28,7 +55,6 @@ double pthreadMultiply(double** matrixA, double** matrixB, double** matrixC, int
 {
   int i;
   printf("Pthread multiply matrix...\n");
-  int num_threads = 2;
   struct thread_data thread_data_array[num_threads];
 
   pthread_t * threads;
@@ -38,6 +64,8 @@ double pthreadMultiply(double** matrixA, double** matrixB, double** matrixC, int
     printf("thread->%d\n", i);
     thread_data_array[i].thread_id = i;
     thread_data_array[i].matrix_a = matrixA;
+    thread_data_array[i].matrix_b = matrixB;
+    thread_data_array[i].matrix_c = matrixC;
 
     pthread_create( &threads[i], NULL, worker, (void *)&thread_data_array[i] );
   }
