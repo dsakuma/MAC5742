48d473c46a51b12b3d6a0d1f2d07dc52f7e8e8f9
Daniel Sakuma <dsakuma@gmail.com>
2018-06-06 11:17:24 -0300

    Accept params

--- ep1/main.c
+++ ep1/main.c
@@ -10,37 +10,63 @@
 
 
 
-int main()
+int main(int argc, char* argv[])
 {
    printf("Hello, main!\n");
-   char filenameA[] = "matrix_a.txt";
-   char filenameB[] = "matrix_b.txt";
 
-   long long int n_rows_a = get_n_rows_or_cols("matrix_a.txt", "rows");
-   long long int n_cols_a = get_n_rows_or_cols("matrix_a.txt", "cols");
-   long long int n_cols_b = get_n_rows_or_cols("matrix_b.txt", "cols");
+   if (argc != 5)
+   {
+       printf("Usage: %s <implementação> <caminho_matr_A> <caminho_matr_B> <caminho_matr_C>\n", argv[0]);
+       return 1;
+   }
+
+   char* implementation = argv[1];
+   char* filenameA = argv[2];
+   char* filenameB = argv[3];
+   char* filenameC = argv[4];
+
+   long long int n_rows_a = get_n_rows_or_cols(filenameA, "rows");
+   long long int n_cols_a = get_n_rows_or_cols(filenameA, "cols");
+   long long int n_cols_b = get_n_rows_or_cols(filenameB, "cols");
 
    double **matrix_a = allocate_memory_and_fill_matrix(filenameA);
    double **matrix_b = allocate_memory_and_fill_matrix(filenameB);
    double **matrix_c = allocate_memory_matrix(n_rows_a, n_cols_b);
 
+   switch (*implementation)
+   {
+     case 's':
+       sequentialMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
+     break;
+
+     case 'o':
+      openmpMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
+     break;
+
+     case 'p':
+       pthreadMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
+     break;
+
+     default:
+       printf("Invalid implementation\n");
+       return 1;
+   }
    // sequentialMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
    // openmpMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
-   pthreadMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
+   // pthreadMultiply(matrix_a, matrix_b, matrix_c, n_rows_a,n_cols_a , n_cols_b);
 
    // print_matrix(matrix_a, n_rows_a, n_cols_a);
    // print_matrix(matrix_b, n_rows_b, n_cols_b);
    // print_matrix(matrix_c, n_rows_a, n_cols_b);
 
-   write_matrix(matrix_c, n_rows_a, n_cols_b, "matrix_c.txt");
+   write_matrix(matrix_c, n_rows_a, n_cols_b, filenameC);
 
    printf("Finished!\n");
-
    return 0;
 }
 
 // Falta:
-// write result matrix
+// input args
 // fix timer
 // write report
 // use intrinsics
