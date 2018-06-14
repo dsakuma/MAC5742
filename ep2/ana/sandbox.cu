219ef1bd6a570470ecf57cd0d1af44d4729e8f4b
Daniel Sakuma <dsakuma@gmail.com>
2018-06-14 16:01:49 -0300

    Send to lab machine

--- ep2/ana/sandbox.cu
+++ ep2/ana/sandbox.cu
@@ -24,8 +24,12 @@ int main(int argc, char *argv[])
 {
     printf("HELLO!!!!");
     int **x;
+    int D = 3;
     int n_els = 9;
     int n_mat = 3;
+
+    FILE *fp;
+    int val1, val2, val3;
 
     // (*input)[i] = (int *) calloc(*n_els, sizeof(int));
     cudaError_t err = cudaMallocManaged(&x, n_els * sizeof(int));
@@ -34,9 +38,20 @@ int main(int argc, char *argv[])
       cudaError_t err = cudaMallocManaged(&x[i], n_mat * sizeof(int));
     }
 
-    for(int i=0; i<n_els; i++ ){
-      for(int j = 0; j<n_mat; j++)
-        x[i][j] = 99;
+    fp = fopen(filename, "r");
+    fscanf(fp, "%d", n_mat);
+    fscanf(fp, "%*s", NULL); // pula linha
+
+    for(int i=0; i < n_mat; i++)
+    {
+      for(int j=0; j < D; j++)
+        {
+            fscanf(fp, "%d %d %d", &val1, &val2, &val3);
+            x[D*j][i] = val1;
+            x[D*j+1][i] = val2;
+            x[D*j+2][i] = val3;
+        }
+        fscanf(fp, "%*s", NULL); // pula linha
     }
 
     print_matrix(x, n_els, n_mat);
