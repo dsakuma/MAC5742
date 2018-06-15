0f635579b1389eb61cde3ec442eb99f85378ad9f
Daniel Sakuma <dsakuma@gmail.com>
2018-06-15 14:26:41 -0300

    Send to lab machine

--- ep2/main.cu
+++ ep2/main.cu
@@ -13,6 +13,8 @@ EP2 - redução em CUDA
 #include <stdio.h>
 #include <sys/time.h>
 
+#ifdef INFINITY
+
 #define CUDA_SAFE_CALL(err) __cuda_safe_call(err, __FILE__, __LINE__)
 
 #define D 3             // dimensão das matrizes (quadradas)
@@ -39,7 +41,7 @@ __global__ void min_kernel(int *result, int **input, int n_mat)
   //input (9x4)
   //tid=0 (prineira thread)
   //index=0 (primeiro elemente cada matriz)
-  //input[index] = [4 1 2 2]
+  //input[index] = [4 1 2]
   //mintile[tid] = 4
   //
 	__shared__ int mintile[1000];
@@ -90,6 +92,11 @@ int main(int argc, char *argv[])
 
     fp = fopen(argv[1], "r");
     fscanf(fp, "%d", &n_mat);
+
+    // tratamento impar
+    // if(n_mat%2 == 1)
+    //   n_mat += 1;
+
     for(int i=0; i < n_els; i++){
       CUDA_SAFE_CALL(cudaMallocManaged(&x[i], n_mat * sizeof(int)));
     }
@@ -99,24 +106,30 @@ int main(int argc, char *argv[])
     for(int i=0; i < n_mat; i++)
     {
       for(int j=0; j < D; j++)
-        {
-            fscanf(fp, "%d %d %d", &val1, &val2, &val3);
-            x[D*j][i] = val1;
-            x[D*j+1][i] = val2;
-            x[D*j+2][i] = val3;
-        }
+      {
+          fscanf(fp, "%d %d %d", &val1, &val2, &val3);
+          x[D*j][i] = val1;
+          x[D*j+1][i] = val2;
+          x[D*j+2][i] = val3;
+      }
         fscanf(fp, "%*s"); // pula linha
     }
 
-
+    //se impar, preenche com inf
+    // for(int j=0; j < D; j++)
+    // {
+    //     x[D*j][n_mat] = INFINITY;
+    //     x[D*j+1][n_mat] = INFINITY;
+    //     x[D*j+2][n_mat] = INFINITY;
+    // }
 
     print_matrix(x, n_els, n_mat);
 
     dim3 numBlocks(D*D);
-    dim3 threadsPerBlock(1);
+    dim3 threadsPerBlock(ceil(n_mat/2));
 
     // <<<number_of_blocks, block_size>>>
-	min_kernel<<<numBlocks, threadsPerBlock>>>(y, x, n_mat);
+	 min_kernel<<<numBlocks, threadsPerBlock>>>(y, x, n_mat);
 
     cudaDeviceSynchronize();
 
