020cf1dca662950a90326bac94202254a5e31310
Daniel Sakuma <dsakuma@gmail.com>
2018-06-17 18:30:48 -0300

    [WIP] Sequential reduction

--- ep2/reduction_seq.cu
+++ ep2/reduction_seq.cu
@@ -14,24 +14,42 @@ int* reduction_seq(char filename[], int matrix_order)
   int* y;
   int n_els = matrix_order*matrix_order;
   int n_mat = get_n_mat(filename);
+  int val1, val2, val3;
 
   /* allocate memory for y */
   y = (int*)malloc(n_els * sizeof(int));
 
 
   /* allocate memory for x */
-  x = (int**)malloc(n_mat*sizeof(double));
+  x = (int**)malloc(n_els*sizeof(int));
   for(int i = 0; i<n_mat; i++)
   {
-    x[i] = (int*)malloc(n_els * sizeof(int));
+    x[i] = (int*)malloc(n_mat * sizeof(int));
   }
 
-  /* initialize with zero */
-  for(int i=0; i<matrix_order; i++){
-    for(int j=0; j<n_els; j++){
-        x[i][j] = 2;
-      }
+  /* read matrix list */
+  fp = fopen(filename, "r");
+  fscanf(fp, "%*s");
+  fscanf(fp, "%*s");
+
+  for(int i=0; i < n_mat; i++)
+  {
+    for(int j=0; j < D; j++)
+    {
+        fscanf(fp, "%d %d %d", &val1, &val2, &val3);
+        x[D*j][i] = val1;
+        x[D*j+1][i] = val2;
+        x[D*j+2][i] = val3;
+    }
+      fscanf(fp, "%*s");  // skip line
   }
+  fclose(fp);
+
+  // for(int i=0; i<matrix_order; i++){
+  //   for(int j=0; j<n_els; j++){
+  //       x[i][j] = 2;
+  //     }
+  // }
 
   print_matrix(x, n_mat, n_els);
 
