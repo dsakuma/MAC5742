15c8c082d34bf18e5b33a7dce951490fe47906e0
Daniel Sakuma <dsakuma@gmail.com>
2018-06-17 01:30:06 -0300

    [WIP] Refactor

--- ep2/reduction_seq.cu
+++ ep2/reduction_seq.cu
@@ -1,1 +1,14 @@
-int* seqReduction(char filename[], int D);
+#include <cuda.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include "functions.h"
+#include "reduction_seq.h"
+
+
+int* seqReduction(char filename[], int D)
+{
+  int *y = { 1, 1, 2, 2, 2, 2, 3, 3, 3 };
+  return y;
+}
--- ep2/reduction_seq.h
+++ ep2/reduction_seq.h
@@ -1,14 +1,1 @@
-#include <cuda.h>
-#include <math.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/time.h>
-#include "functions.h"
-#include "reduction_seq.h"
-
-
-int* seqReduction(char filename[], int D)
-{
-  int *y = { 1, 1, 2, 2, 2, 2, 3, 3, 3 };
-  return y;
-}
+int* seqReduction(char filename[], int D);
