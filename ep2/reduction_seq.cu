166629d526a2e97907a9d691ca99d65038c6ef4f
Daniel Sakuma <dsakuma@gmail.com>
2018-06-17 14:55:52 -0300

    [WIP] Sequential reduction

--- ep2/reduction_seq.cu
+++ ep2/reduction_seq.cu
@@ -7,21 +7,40 @@
 #include "reduction_seq.h"
 
 
-int* reduction_seq(char filename[], int D)
+int* reduction_seq(char filename[], int matrix_order)
 {
+  printf("Start reduction seq\n");
+  int **x;
   int* y;
-  int n_els = D*D;
+  int n_els = matrix_order*matrix_order;
   int n_mat = get_n_mat(filename);
 
-  printf("n_mat: %d", n_mat);
-
-  /* allocate memory */
+  /* allocate memory for y */
   y = (int*)malloc(n_els * sizeof(int));
 
-  /* initialize with zero */
-  for(int i=0; i<n_els; i++){
-    y[i] = 0;
+
+  /* allocate memory for x */
+  x = malloc(n_mat*sizeof(double*));
+  for(int i = 0; i<n_mat; i++)
+  {
+    x[i] = (int*)malloc(n_els * sizeof(int));
   }
 
+  /* initialize with zero */
+  for(int i=0; i<matrix_order; i++){
+    for(int j=0; j<matrix_order; j++){
+        x[i][j] = 0;
+      }
+  }
+
+  print_matrix(x, n_mat, n_els);
+
+
+  // /* initialize with zero */
+  // for(int i=0; i<n_els; i++){
+  //   y[i] = 0;
+  // }
+
+  printf("Finish reduction seq\n");
   return y;
 }
