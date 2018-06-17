bc4e4af951a264dec165a069cd7eccbcb7966234
Daniel Sakuma <dsakuma@gmail.com>
2018-06-17 01:52:03 -0300

    [WIP] Refactor

--- ep2/reduction_seq.cu
+++ ep2/reduction_seq.cu
@@ -13,7 +13,7 @@ int* seqReduction(char filename[], int D)
   int n_els = D*D;
 
   /* allocate memory */
-  y = malloc(n_els*sizeof(int*);
+  y = malloc(n_els*sizeof(int*));
 
   /* initialize with zero */
   for(int i=0; i<n_els; i++){
