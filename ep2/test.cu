d4206c81f3e502d58b1488891abd7f7ff70b504e
Daniel Sakuma <dsakuma@gmail.com>
2018-06-18 19:20:04 -0300

    [WIP] Fix cuda

--- ep2/test.cu
+++ ep2/test.cu
@@ -61,10 +61,22 @@ int main(int argc, char *argv[])
   result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
   print_test_result(description, result);
 
-  /* Teste 5: Quantidade par de matrizes */
+  /* Teste 5: 10 matrizes */
   description = "Dez matrizes (a última thread não tem com o que comparar)";
   //given
   filename = "data/teste_10.txt";
+  //when
+  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  y_seq = reduction_seq(filename, MATRIX_ORDER);
+  //then
+  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  print_test_result(description, result);
+
+  /* Teste 6: 10k matrizes */
+  description = "Dez matrizes (a última thread não tem com o que comparar)";
+  //given
+  filename = "data/teste_10k.txt";
+  write_matrix_list(10000, filename, MATRIX_ORDER);
   //when
   y_cuda = reduction_cuda(filename, MATRIX_ORDER);
   y_seq = reduction_seq(filename, MATRIX_ORDER);
