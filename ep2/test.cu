08f933e6935a5f9297eb98782ed3eabf8ae46504
Daniel Sakuma <dsakuma@gmail.com>
2018-06-18 15:20:46 -0300

    [WIP] Add performance tests

--- ep2/test.cu
+++ ep2/test.cu
@@ -17,7 +17,29 @@ int main(int argc, char *argv[])
   int *y_cuda;
   int *y_seq ;
 
-  /* Teste 1: Quantidade ímpar de matrizes */
+  /* Teste 1: Uma matriz */
+  description = "Uma matriz";
+  //given
+  filename = "data/teste_1.txt";
+  //when
+  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  y_seq = reduction_seq(filename, MATRIX_ORDER);
+  //then
+  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  print_test_result(description, result);
+
+  /* Teste 2: Duas matrizes */
+  description = "Duas matrizes";
+  //given
+  filename = "data/teste_2.txt";
+  //when
+  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  y_seq = reduction_seq(filename, MATRIX_ORDER);
+  //then
+  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  print_test_result(description, result);
+
+  /* Teste 3: Quantidade ímpar de matrizes */
   description = "Quantidade ímpar de matrizes";
   //given
   filename = "data/teste_impar.txt";
@@ -28,7 +50,7 @@ int main(int argc, char *argv[])
   result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
   print_test_result(description, result);
 
-  /* Teste 2: Quantidade par de matrizes */
+  /* Teste 4: Quantidade par de matrizes */
   description = "Quantidade par de matrizes";
   //given
   filename = "data/teste_par.txt";
