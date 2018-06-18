e40996864a92604271642693770442e839335e4e
Daniel Sakuma <dsakuma@gmail.com>
2018-06-18 18:41:29 -0300

    [WIP] Fix cuda

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -45,7 +45,7 @@ __global__ void min_kernel(int *result, int **input, int n_mat)
 	{
     int idx = 2*s*tid;
     if(tid == 2 && index ==1)
-      printf("index=%d (bloco), tid=%d (n_mat), idx=%d, blockDim.x-1=%d\n", index, tid, idx,blockDim.x-1);
+      printf("index=%d (bloco), tid=%d (n_mat), idx=%d, blockDim.x-1=%d\n", index, tid, idx,blockDim.x);
 		if (idx < blockDim.x-1)
 		{
       if(tid == 2 && index ==1)
