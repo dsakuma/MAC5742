798c68f23faf4b5e14b03f0804d3fa8bfad4ac48
Daniel Sakuma <dsakuma@gmail.com>
2018-06-20 16:54:17 -0300

    [WIP] Implement block reduction

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -3,13 +3,14 @@
 #include <cuda_runtime.h>
 
 
-int main(int argc, char **argv)
+int* reduction_cuda(const char filename[], int D)
 {
+    int *y;
     int deviceCount;
     cudaGetDeviceCount(&deviceCount);
     if (deviceCount == 0) {
         fprintf(stderr, "error: no devices supporting CUDA.\n");
         exit(EXIT_FAILURE);
     }
-    return 0;
+    return y;
 }
