88253826b5451b800d2cdb526aeff06f03da7a62
Daniel Sakuma <dsakuma@gmail.com>
2018-06-20 17:51:47 -0300

    [WIP] Implement block reduction

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -1,38 +1,87 @@
-#include <stdio.h>
+#include <cuda.h>
+#include <math.h>
 #include <stdlib.h>
-#include <cuda_runtime.h>
+#include <stdio.h>
+#include <sys/time.h>
+#include "functions.h"
+#include "reduction_cuda.h"
 
+__global__ void min_kernel(int *result, int **input, int n_mat)
+{
+	__shared__ int mintile[9];
+
+	unsigned int tid = threadIdx.x;
+	unsigned int index = blockIdx.x;
+	mintile[tid] = input[index][tid];
+
+  // printf("index=%d (bloco), tid=%d (n_mat), part_min=%d, blockDim.x-1=%d\n", index, tid, mintile[tid], blockDim.x);
+
+  __syncthreads();
+
+	// strided index and non-divergent branch
+	for (unsigned int s = 1; s < blockDim.x; s *= 2)
+	{
+    int idx = 2*s*tid;
+    // if(tid == 2 && index ==1)
+    //   printf("index=%d (bloco), tid=%d (n_mat), idx=%d, blockDim.x-1=%d\n", index, tid, idx,blockDim.x);
+		if (idx+s < blockDim.x)
+		{
+      // if(tid == 2 && index ==1)
+      //   printf("mintile[idx]=%d, mintile[idx+s]=%d\n", mintile[idx], mintile[idx + s]);
+
+			if (mintile[idx + s] < mintile[idx])
+      {
+        mintile[idx] = mintile[idx + s];
+      }
+		}
+		__syncthreads();
+	}
+
+	if (tid == 0)
+	{
+		result[index] = mintile[0];
+	}
+}
 
 int* reduction_cuda(const char filename[], int D)
 {
-    int *y;
+  int **x;
+  int *y;
+  int n_els = D*D;
+  int n_mat;
 
-    const int ARRAY_SIZE = 1 << 20;
-    const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
+  FILE *fp;
+  int val1, val2, val3;
 
-    // generate the input array on the host
-    float h_in[ARRAY_SIZE];
-    float sum = 0.0f;
-    for(int i = 0; i < ARRAY_SIZE; i++) {
-        // generate random float in [-1.0f, 1.0f]
-        h_in[i] = -1.0f + (float)random()/((float)RAND_MAX/2.0f);
-        sum += h_in[i];
+  cudaMallocManaged(&x, n_els * sizeof(int*));
+  cudaMallocManaged(&y, n_els * sizeof(int));
+
+  fp = fopen(filename, "r");
+  fscanf(fp, "%d", &n_mat);
+
+  for(int i=0; i < n_els; i++){
+    cudaMallocManaged(&x[i], n_mat * sizeof(int));
+  }
+
+  fscanf(fp, "%*s"); // skip line
+
+  for(int i=0; i < n_mat; i++)
+  {
+    for(int j=0; j < D; j++)
+    {
+        fscanf(fp, "%d %d %d", &val1, &val2, &val3);
+        x[D*j][i] = val1;
+        x[D*j+1][i] = val2;
+        x[D*j+2][i] = val3;
     }
+      fscanf(fp, "%*s");  // skip line
+  }
 
-    // declare GPU memory pointers
-    float * d_in, * d_intermediate, * d_out;
+  dim3 numBlocks(D*D);
+  dim3 threadsPerBlock(n_mat);
 
-    // allocate GPU memory
-    cudaMalloc((void **) &d_in, ARRAY_BYTES);
-    cudaMalloc((void **) &d_intermediate, ARRAY_BYTES); // overallocated
-    cudaMalloc((void **) &d_out, sizeof(float));
+  min_kernel<<<numBlocks, threadsPerBlock>>>(y, x, n_mat); //<<<number_of_blocks, block_size>>>
 
-    // transfer the input array to the GPU
-    cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
-
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-
-    return y;
+  cudaDeviceSynchronize();
+  return y;
 }
