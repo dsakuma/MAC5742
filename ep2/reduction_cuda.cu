fb17371b65d5ca0d75b7d8f7807dffd4e8adb28d
Daniel Sakuma <dsakuma@gmail.com>
2018-06-20 16:56:52 -0300

    [WIP] Implement block reduction

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -12,5 +12,17 @@ int* reduction_cuda(const char filename[], int D)
         fprintf(stderr, "error: no devices supporting CUDA.\n");
         exit(EXIT_FAILURE);
     }
+    int dev = 0;
+    cudaSetDevice(dev);
+
+    cudaDeviceProp devProps;
+    if (cudaGetDeviceProperties(&devProps, dev) == 0)
+    {
+        printf("Using device %d:\n", dev);
+        printf("%s; global mem: %dB; compute v%d.%d; clock: %d kHz\n",
+               devProps.name, (int)devProps.totalGlobalMem,
+               (int)devProps.major, (int)devProps.minor,
+               (int)devProps.clockRate);
+    }    
     return y;
 }
