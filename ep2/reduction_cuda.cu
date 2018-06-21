e2a72e5b22665a15ee132aa3704a0b6e37a8ce92
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 00:03:37 -0300

    [WIP] Implement block reduction

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -46,9 +46,9 @@ __global__ void min_kernel(int *result, int **input, int n_mat)
 
 		if (idx+s < blockDim.x  && start+s < n_mat)
 		{
-      // if(index_x ==0 && index_y==1)
-      //   printf("index_x=%d (elem of mat), index_y=%d (which partition), tid=%d (max 256), idx=%d, blockDim.x=%d, s=%d, mintile[idx]=%d, mintile[idx+s]=%d\n",
-      //           index_x, index_y, tid, idx,blockDim.x, s, mintile[idx], mintile[idx + s]);
+      if(index_x ==0 && index_y==1)
+        printf("index_x=%d (elem of mat), index_y=%d (which partition), tid=%d (max 256), idx=%d, blockDim.x=%d, s=%d, mintile[idx]=%d, mintile[idx+s]=%d\n",
+                index_x, index_y, tid, idx,blockDim.x, s, mintile[idx], mintile[idx + s]);
 
 			if (mintile[idx + s] < mintile[idx])
       {
