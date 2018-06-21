1b4b375a0bffb42bc93b290579e34609450d2b91
Daniel Sakuma <dsakuma@gmail.com>
2018-06-20 23:32:06 -0300

    [WIP] Implement block reduction

--- ep2/test.cu
+++ ep2/test.cu
@@ -61,17 +61,17 @@ int main(int argc, char *argv[])
   result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
   print_test_result(description, result);
 
-  // /* Teste 5: 10 matrizes */
-  // description = "Dez matrizes (a última thread não tem com o que comparar)";
-  // //given
-  // filename = "data/teste_10.txt";
-  // //when
-  // y_cuda = reduction_cuda(filename, MATRIX_ORDER);
-  // y_seq = reduction_seq(filename, MATRIX_ORDER);
-  // //then
-  // result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
-  // print_test_result(description, result);
-  //
+  /* Teste 5: 10 matrizes */
+  description = "Dez matrizes (a última thread não tem com o que comparar)";
+  //given
+  filename = "data/teste_10.txt";
+  //when
+  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  y_seq = reduction_seq(filename, MATRIX_ORDER);
+  //then
+  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  print_test_result(description, result);
+
   // /* Teste 6: 65 matrizes */
   // description = "65 matrizes (acontece bus error)";
   // //given
