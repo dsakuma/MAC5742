88818b45437edc041ca60922fa0bb31bd6198b03
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 15:48:36 -0300

    [WIP] Implement block reduction for

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -23,9 +23,9 @@ __global__ void min_kernel(int **result, int **input, int n_mat)
 
 	mintile[tid] = input[index_x][start];
 
-  if(index_x ==0 )
-	  printf("index_x=%d (elem of mat),  index_y=%d (partition), tid=%d (max 256), mintile[tid]=%d\n",
-	          index_x, index_y, tid, mintile[tid]);
+  // if(index_x ==0 )
+	//   printf("index_x=%d (elem of mat),  index_y=%d (partition), tid=%d (max 256), mintile[tid]=%d\n",
+	//           index_x, index_y, tid, mintile[tid]);
   //idx -> start
   //idx+s -> end
 
@@ -35,9 +35,9 @@ __global__ void min_kernel(int **result, int **input, int n_mat)
 	for (unsigned int s = 1; s < blockDim.x; s *= 2)
 	{
     int idx = 2*s*tid;
-    // if(index_x ==0 && index_y==0)
-      // printf("index_x=%d (elem of mat), index_y=%d (partition), tid=%d (max 256), idx=%d, blockDim.x=%d, s=%d\n",
-              // index_x, index_y, tid, idx,blockDim.x, s);
+    if(index_x ==0 && index_y==0)
+      printf("index_x=%d (elem of mat), index_y=%d (partition), tid=%d (max 256), idx=%d, blockDim.x=%d, s=%d\n",
+              index_x, index_y, tid, idx,blockDim.x, s);
 
     // if(index_x ==0 && index_y==0)
     //   printf("+++ idx+s=%d, blockDim.x=%d, n_mat=%d", idx+s, blockDim.x, n_mat);
