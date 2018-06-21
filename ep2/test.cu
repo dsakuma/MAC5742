d7cf939b61e700618b265c86748221657b8b08d6
Daniel Sakuma <dsakuma@gmail.com>
2018-06-20 23:30:55 -0300

    [WIP] Implement block reduction

--- ep2/test.cu
+++ ep2/test.cu
@@ -61,64 +61,64 @@ int main(int argc, char *argv[])
   result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
   print_test_result(description, result);
 
-  /* Teste 5: 10 matrizes */
-  description = "Dez matrizes (a última thread não tem com o que comparar)";
-  //given
-  filename = "data/teste_10.txt";
-  //when
-  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
-  y_seq = reduction_seq(filename, MATRIX_ORDER);
-  //then
-  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
-  print_test_result(description, result);
-
-  /* Teste 6: 65 matrizes */
-  description = "65 matrizes (acontece bus error)";
-  //given
-  filename = "data/teste_65.txt";
-  write_matrix_list(65, filename, MATRIX_ORDER);
-  //when
-  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
-  y_seq = reduction_seq(filename, MATRIX_ORDER);
-  //then
-  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
-  print_test_result(description, result);
-
-  /* Teste 7: 1024 matrizes */
-  description = "1024 matrizes (n max de threads por bloco)";
-  //given
-  filename = "data/teste_1024.txt";
-  write_matrix_list(1024, filename, MATRIX_ORDER);
-  //when
-  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
-  y_seq = reduction_seq(filename, MATRIX_ORDER);
-  //then
-  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
-  print_test_result(description, result);
-
-  /* Teste 8: 1025 matrizes */
-  description = "1025 matrizes (ultrapassa n max de threads por bloco)";
-  //given
-  filename = "data/teste_1025.txt";
-  write_matrix_list(1025, filename, MATRIX_ORDER);
-  //when
-  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
-  y_seq = reduction_seq(filename, MATRIX_ORDER);
-  //then
-  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
-  print_test_result(description, result);
-
-  /* Teste 9: 10k matrizes */
-  description = "10k matrizes (usa bastante memória da gpu)";
-  //given
-  filename = "data/teste_10k.txt";
-  write_matrix_list(10000, filename, MATRIX_ORDER);
-  //when
-  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
-  y_seq = reduction_seq(filename, MATRIX_ORDER);
-  //then
-  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
-  print_test_result(description, result);
+  // /* Teste 5: 10 matrizes */
+  // description = "Dez matrizes (a última thread não tem com o que comparar)";
+  // //given
+  // filename = "data/teste_10.txt";
+  // //when
+  // y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  // y_seq = reduction_seq(filename, MATRIX_ORDER);
+  // //then
+  // result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  // print_test_result(description, result);
+  //
+  // /* Teste 6: 65 matrizes */
+  // description = "65 matrizes (acontece bus error)";
+  // //given
+  // filename = "data/teste_65.txt";
+  // write_matrix_list(65, filename, MATRIX_ORDER);
+  // //when
+  // y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  // y_seq = reduction_seq(filename, MATRIX_ORDER);
+  // //then
+  // result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  // print_test_result(description, result);
+  //
+  // /* Teste 7: 1024 matrizes */
+  // description = "1024 matrizes (n max de threads por bloco)";
+  // //given
+  // filename = "data/teste_1024.txt";
+  // write_matrix_list(1024, filename, MATRIX_ORDER);
+  // //when
+  // y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  // y_seq = reduction_seq(filename, MATRIX_ORDER);
+  // //then
+  // result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  // print_test_result(description, result);
+  //
+  // /* Teste 8: 1025 matrizes */
+  // description = "1025 matrizes (ultrapassa n max de threads por bloco)";
+  // //given
+  // filename = "data/teste_1025.txt";
+  // write_matrix_list(1025, filename, MATRIX_ORDER);
+  // //when
+  // y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  // y_seq = reduction_seq(filename, MATRIX_ORDER);
+  // //then
+  // result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  // print_test_result(description, result);
+  //
+  // /* Teste 9: 10k matrizes */
+  // description = "10k matrizes (usa bastante memória da gpu)";
+  // //given
+  // filename = "data/teste_10k.txt";
+  // write_matrix_list(10000, filename, MATRIX_ORDER);
+  // //when
+  // y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  // y_seq = reduction_seq(filename, MATRIX_ORDER);
+  // //then
+  // result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  // print_test_result(description, result);
 
   return 0;
 }
