16a9e6a42b94664073888bc2b2b319408a3b0849
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 20:22:14 -0300

    [WIP] Optmize reduce

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -112,21 +112,21 @@ int* reduction_cuda(const char filename[], int D)
 		min_kernel<<<numBlocks, threadsPerBlock>>>(y, x, n_mat); //<<<number_of_blocks, block_size>>>
 		cudaDeviceSynchronize();
 		n_mat = n_partitions;
-		// tmp = x;
-		// x = y;
-		// y = tmp;
+		tmp = x;
+		x = y;
+		y = tmp;
 
-		for(int i=0; i < n_els; i++)
-		{
-			for(int j=0; j < n_mat; j++)
-					x[i][j] = y[i][j];
-		}
+		// for(int i=0; i < n_els; i++)
+		// {
+		// 	for(int j=0; j < n_mat; j++)
+		// 			x[i][j] = y[i][j];
+		// }
 
 	}while(n_partitions > 1);
 
 	res = (int*) calloc(n_els, sizeof(int));
 	for(int i=0; i < n_els; i++)
-			res[i] = y[i][0];
+			res[i] = x[i][0];
 
 	return res;
 }
