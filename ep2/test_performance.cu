a5a99fb5338a360b3cb21993ba14422d4bb9ce6d
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 00:13:23 -0300

    [WIP] Implement block reduction

--- ep2/test_performance.cu
+++ ep2/test_performance.cu
@@ -15,6 +15,7 @@ int main(int argc, char *argv[])
   const char* description;
   struct timeval t0, t1, t2;
 
+  /* Teste 1: Redução de 512 matrizes */
   description = "Redução de 512 matrizes";
   //given
   filename = "data/teste_512.txt";
@@ -28,7 +29,7 @@ int main(int argc, char *argv[])
   //then
   print_performance_test_result(description, time_elapsed(t0, t1), time_elapsed(t1, t2));
 
-  /* Teste 1: Redução de 2048 matrizes */
+  /* Teste 2: Redução de 2048 matrizes */
   description = "Redução de 2048 matrizes";
   //given
   filename = "data/teste_2k.txt";
@@ -42,7 +43,7 @@ int main(int argc, char *argv[])
   //then
   print_performance_test_result(description, time_elapsed(t0, t1), time_elapsed(t1, t2));
 
-  /* Teste 2: Redução de 8192 matrizes */
+  /* Teste 3: Redução de 8192 matrizes */
   description = "Redução de 8192 matrizes";
   //given
   filename = "data/teste_8k.txt";
@@ -56,7 +57,7 @@ int main(int argc, char *argv[])
   //then
   print_performance_test_result(description, time_elapsed(t0, t1), time_elapsed(t1, t2));
 
-  /* Teste 3: Redução de 1M matrizes */
+  /* Teste 4: Redução de 1M matrizes */
   description = "Redução de 1M matrizes";
   //given
   filename = "data/teste_1M.txt";
@@ -70,19 +71,19 @@ int main(int argc, char *argv[])
   //then
   print_performance_test_result(description, time_elapsed(t0, t1), time_elapsed(t1, t2));
 
-  /* Teste 4: Redução de 10B matrizes */
-  // description = "Redução de 10B matrizes";
-  // //given
-  // filename = "data/teste_10B.txt";
-  // write_matrix_list(1<<30, filename, MATRIX_ORDER);
-  // //when
-  // gettimeofday(&t0, NULL);
-  // reduction_cuda(filename, MATRIX_ORDER);
-  // gettimeofday(&t1, NULL);
-  // reduction_seq(filename, MATRIX_ORDER);
-  // gettimeofday(&t2, NULL);
-  // //then
-  // print_performance_test_result(description, time_elapsed(t0, t1), time_elapsed(t1, t2));
+  /* Teste 5: Redução de 10B matrizes */
+  description = "Redução de 10M matrizes";
+  //given
+  filename = "data/teste_10M.txt";
+  write_matrix_list(10000000, filename, MATRIX_ORDER);
+  //when
+  gettimeofday(&t0, NULL);
+  reduction_cuda(filename, MATRIX_ORDER);
+  gettimeofday(&t1, NULL);
+  reduction_seq(filename, MATRIX_ORDER);
+  gettimeofday(&t2, NULL);
+  //then
+  print_performance_test_result(description, time_elapsed(t0, t1), time_elapsed(t1, t2));
 
   return 0;
 }
