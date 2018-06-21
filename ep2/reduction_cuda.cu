c37157ca8e4d92ce71953394f473c968bb1ca0b9
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 00:34:35 -0300

    [WIP] Implement block reduction

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -6,7 +6,7 @@
 #include "functions.h"
 #include "reduction_cuda.h"
 
-#define THREADS_PER_BLOCK 1024
+#define THREADS_PER_BLOCK 256
 
 __global__ void min_kernel(int *result, int **input, int n_mat)
 {
