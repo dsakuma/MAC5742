c15ec8fe0b145e815e148104d2669a51b4fc5cb6
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 15:46:05 -0300

    [WIP] Implement block reduction for

--- ep2/reduction_cuda.cu
+++ ep2/reduction_cuda.cu
@@ -122,7 +122,7 @@ int* reduction_cuda(const char filename[], int D)
 		n_partitions = (int)ceil(n_mat/(float)THREADS_PER_BLOCK);
 		dim3 numBlocks(n_els, n_partitions);
 		dim3 threadsPerBlock(THREADS_PER_BLOCK);
-		printf("Chamando o kernel: numBlocks=%d, threadsPerBlock=%d, n_mat=%d\n", numBlocks,threadsPerBlock, n_mat);
+		printf("Chamando o kernel: n_els=%d, n_partitions=%d, n_mat=%d\n", n_els, n_partitions, n_mat);
 		min_kernel<<<numBlocks, threadsPerBlock>>>(y, x, n_mat); //<<<number_of_blocks, block_size>>>
 		cudaDeviceSynchronize();
 		n_mat = n_partitions;
