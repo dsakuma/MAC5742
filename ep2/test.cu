2291b50d313c0c5c54dd39253fec139b1aab11d4
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 16:43:26 -0300

    [WIP] Implement block reduction for

--- ep2/test.cu
+++ ep2/test.cu
@@ -121,5 +121,28 @@ int main(int argc, char *argv[])
   result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
   print_test_result(description, result);
 
+  /* Teste 10: 1M matrizes */
+  description = "1M matrizes (usa bastante memória da gpu)";
+  //given
+  filename = "data/teste_1M.txt";
+  write_matrix_list(1000000, filename, MATRIX_ORDER);
+  //when
+  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  y_seq = reduction_seq(filename, MATRIX_ORDER);
+  //then
+  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  print_test_result(description, result);
+
+  /* Teste 11: 10M matrizes */
+  description = "1M matrizes (usa bastante memória da gpu)";
+  //given
+  filename = "data/teste_10M.txt";
+  write_matrix_list(10000000, filename, MATRIX_ORDER);
+  //when
+  y_cuda = reduction_cuda(filename, MATRIX_ORDER);
+  y_seq = reduction_seq(filename, MATRIX_ORDER);
+  //then
+  result = assert_vector(y_cuda, y_seq, MATRIX_ORDER*MATRIX_ORDER);
+  print_test_result(description, result);
   return 0;
 }
