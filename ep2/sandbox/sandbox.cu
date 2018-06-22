1a0cbf68fd2d915f97aa1817083abde58e907584
Daniel Sakuma <dsakuma@gmail.com>
2018-06-21 21:08:10 -0300

    [WIP] Optmize reduce

--- ep2/sandbox/sandbox.cu
+++ ep2/sandbox/sandbox.cu
@@ -20,16 +20,19 @@ int main(int argc, char *argv[])
   ptx = &a;
   pty = &b;
 
+  printf("\n[ptx ]:Value of ptx = %p", ptx);
+  printf("\n[ptx ]:Value of pty = %p", pty);
+
   pttmp = ptx;
   ptx = pty;
   pty = ptx;
 
 
-  printf("\n[a  ]:Value of A = %d", a);
-  printf("\n[*ptx]:Value of A = %d", *ptx);
-  printf("\n[&a ]:Address of A = %p", &a);
-  printf("\n[ptx ]:Address of A = %p", ptx);
-  printf("\n[&ptx]:Address of ptx = %p", &ptx);
+  // printf("\n[a  ]:Value of A = %d", a);
+  // printf("\n[*ptx]:Value of A = %d", *ptx);
+  // printf("\n[&a ]:Address of A = %p", &a);
+  // printf("\n[ptx ]:Address of A = %p", ptx);
+  // printf("\n[&ptx]:Address of ptx = %p", &ptx);
   printf("\n[ptx ]:Value of ptx = %p", ptx);
   printf("\n[ptx ]:Value of pty = %p", pty);
 
